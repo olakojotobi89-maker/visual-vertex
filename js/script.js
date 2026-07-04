810f4852b2685326216f473b9176efc22f95976a
olakojotobi89 <olakojotobi89@gmail.com>
2026-07-04 15:21:44 +0100

    Latests updates

--- js/script.js
+++ js/script.js
@@ -734,3 +734,10 @@ document.addEventListener("mouseleave", () => {
     heroLogo.style.transform = "";
 
 });
+
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
