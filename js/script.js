48d72d8c0b916869f89f01a48831cc66bb453f0d
olakojotobi89 <olakojotobi89@gmail.com>
2026-07-06 07:11:30 +0100

    script.js

--- js/script.js
+++ js/script.js
@@ -742,8 +742,6 @@ const navLinks = document.querySelector(".nav-links");
 Mobile Menu
 ==========================================*/
 
-const menuBtn = document.querySelector(".menu-btn");
-const navLinks = document.querySelector(".nav-links");
 
 menuBtn.addEventListener("click", () => {
 
